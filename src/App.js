c8c9b046f8cd5f86e224145eecb0c50ce1a4c064
Peter Conley <peterdavidconley@gmail.com>
2022-02-28 08:36:34 -0500

    wrote new scaffolding for App.js and made axiosWithAuth file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// *** ----- Styling  ----- ***
+
+// const AppContainer = styled.div`
+// 	height: 100%;
+// `;
+// const RouteContainer = styled.div`
+// 	display: flex;
+// 	height: 85%;
+// 	align-items: center;
+// 	flex-direction: column;
+// `;
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>BTC Net Worth Application</h1>
       </header>
     </div>
   );
