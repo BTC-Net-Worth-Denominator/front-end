25040602b6456d3338c6789451dcd83800eeb851
Peter Conley <peterdavidconley@gmail.com>
2022-03-02 12:15:39 -0500

    added scaffolding for CreateAccount.js

--- src/Components/CreateAccount.js
+++ src/Components/CreateAccount.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const initialCredentials = {
+    username: '',
+    password: '',
+}
+
+const initialMessage = ''
 
 const CreateAccount = () => {
 
+    const [ credentials, setCredentials ] = useState(initialCredentials);
+    const [ loginMessage, setLoginMessage ] = useState(initialMessage);
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        // axios.post(`https://potluckplan.herokuapp.com/api/auth/register`, credentials)
+        // .then(resp => {
+        //     console.log(resp)
+        //     setLoginMessage(resp.data.message)
+        // })
+        // .catch(err => {
+        //     console.log(err)
+        // })
+
+    }
+
     return(
         <div>
-            <h2>Create Account Component</h2>
+            <center><h1>Create Account</h1></center>
+            <form onSubmit={handleSubmit}>
+                <label>Enter Username:
+                    <input 
+                    type="text" 
+                    name="username" 
+                    id="username"
+                    onChange={handleChange}
+                    />
+                </label>
+                <label>Create Password:
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="password"
+                    onChange={handleChange}
+                    />
+                </label>
+                <button id='submit'>Submit</button>
+            </form>
+            <center>
+                <h3>Already A User?</h3>    
+                <Link to="/login" >
+                <h4>Login Page</h4>
+                </Link>
+            </center>
         </div>
     )
 }
