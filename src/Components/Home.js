e2e7226c57b73944285c91fd9bc38c006ddea68c
Peter Conley <peterdavidconley@gmail.com>
2022-03-06 18:03:12 -0500

    removed spacing inbetween links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,7 +18,6 @@ const Home = () => {
             <img src='https://peterdavidconley.com/wp-content/uploads/2022/03/bitcoin_logo.png' height='150px' width='150px'></img>
             <h1>BTC Net Worth Calculator</h1>
             <h3 style={{color: 'orange'}}>Calculate your assets worth in sats.</h3>
-            <br />
             <Link to="/create-account" >
             <h4 style={{color: 'grey'}}>Create Account</h4>
             </Link>
