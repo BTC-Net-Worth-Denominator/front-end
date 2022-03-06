3cb55ad59d9a61c7f684a8e4b2d479dbbc2f576b
Peter Conley <peterdavidconley@gmail.com>
2022-03-06 18:02:32 -0500

    homepage styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HomeStyling = styled.div`
+	padding: 5%;
+    font-size: 30px;
+    a:link {
+        color: grey;
+        }
+`;
 
 const Home = () => {
 
     return(
+        <HomeStyling>
         <div>
-            <img>{/*bitcoin logo here*/}</img>
+            <img src='https://peterdavidconley.com/wp-content/uploads/2022/03/bitcoin_logo.png' height='150px' width='150px'></img>
             <h1>BTC Net Worth Calculator</h1>
-            <br />
-            <h3>Calculate your asset's worth in sats.</h3>
+            <h3 style={{color: 'orange'}}>Calculate your assets worth in sats.</h3>
             <br />
             <Link to="/create-account" >
-            <h4>Create Account</h4>
+            <h4 style={{color: 'grey'}}>Create Account</h4>
             </Link>
             <Link to="/login" >
-            <h4>Login</h4>
+            <h4 style={{color: 'grey'}}>Login</h4>
             </Link>
         </div>
+        </HomeStyling>
     )
 }
 
