73dc3529ad5baa72d09b8612ba653cb0ccc6af0b
Peter Conley <peterdavidconley@gmail.com>
2022-03-17 10:30:36 -0400

    styled number components on Process.js

--- src/Components/TheProcess.js
+++ src/Components/TheProcess.js
@@ -11,6 +11,14 @@ const ProcessStyling = styled.div`
     padding: 4% 0%;
 `
 
+const NumberStyling = styled.div`
+    background-color: orange;
+    border-radius: 50%;
+    padding: 2%;
+    margin: 5%;
+    color: black;
+`
+
 const Process = () => {
 
 
@@ -19,22 +27,30 @@ const Process = () => {
             <center><h1>The Process</h1></center>
             <br />
             <StepContainer>
-                <h1>1</h1>
+                <NumberStyling>
+                    <h1>1</h1>  
+                </NumberStyling>
                 <h3>Create an account</h3>
             </StepContainer>
             <br />
             <StepContainer>
-                <h1>2</h1>
+                <NumberStyling>
+                    <h1>2</h1>
+                </NumberStyling>
                 <h3>Add assets to your portfolio</h3>
             </StepContainer>
             <br />
             <StepContainer>
-                <h1>3</h1>
+                <NumberStyling>
+                    <h1>3</h1>
+                </NumberStyling>
                 <h3>Assign a USD value to your asset</h3>
             </StepContainer>
             <br />
             <StepContainer>
-                <h1>4</h1>
+                <NumberStyling>
+                    <h1>4</h1>
+                </NumberStyling>
                 <h3>View entire net worth denominated in BTC</h3>
             </StepContainer>
         </ProcessStyling>
