d317c7da3d682808730fb45c9925102a78600b10
Peter Conley <peterdavidconley@gmail.com>
2022-03-18 16:41:18 -0400

    finished syling links on Home.js component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,10 +5,13 @@ import styled from 'styled-components';
 const HomeStyling = styled.div`
 	padding: 5%;
     font-size: 30px;
-    a:link {
-        color: grey;
-        }
 `;
+
+const LinkStyling = styled.div`
+    color: grey;
+    text-decoration: underline;
+    text-decoration-color: grey;
+`
 
 const Home = () => {
 
@@ -18,12 +21,16 @@ const Home = () => {
             <img src='https://peterdavidconley.com/wp-content/uploads/2022/03/bitcoin_logo.png' height='150px' width='150px'></img>
             <h1>BTC Net Worth Calculator</h1>
             <h3 style={{color: 'orange'}}>Calculate your assets worth in sats.</h3>
-            <Link to="/create-account" >
-            <h4 style={{color: 'grey'}}>Create Account</h4>
-            </Link>
-            <Link to="/login" >
-            <h4 style={{color: 'grey'}}>Login</h4>
-            </Link>
+                <Link to="/create-account" >
+                    <LinkStyling>
+                    <h4>Create Account</h4>
+                    </LinkStyling>
+                </Link>
+                <Link to="/login" >
+                    <LinkStyling>
+                    <h4>Login</h4>
+                    </LinkStyling>
+                </Link>
         </div>
         </HomeStyling>
     )
