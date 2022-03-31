e0dde7f63283615bb14b5041f0d0ca65f208f6be
Peter Conley <peterdavidconley@gmail.com>
2022-03-31 08:52:23 -0400

    wrote handleDelete function on asset [x]

--- src/Components/Asset.js
+++ src/Components/Asset.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
+import axiosWithAuth from '../Utils/axiosWithAuth';
+
 
 const AssetStyling = styled.div`
     display: flex;
@@ -12,10 +14,21 @@ const Asset = (props) => {
 
     const { asset } = props
 
+    const handleDelete = () => {
+        axiosWithAuth()
+        .delete(`/asset/${asset.asset_id}`)
+        .then(resp => {
+            console.log(resp)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
     return(
         <div >
             <AssetStyling>
-                <h2>[ X ]</h2>
+                <h2 >[ X ]</h2>
                 <h2> {/* asset.asset_name */} Asset Name</h2>
                 <h2> {/* assets.asset_price */} BTC</h2>
             </AssetStyling>
